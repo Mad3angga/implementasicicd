354e594cb35ea2ecfc503a30589cba192ecfecab
Made Angga Danan Jaya <88696627+Mad3angga@users.noreply.github.com>
2023-12-30 16:23:39 +0800

    Update index.js  2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/will', function (req, res) {
     res.send('{ "response": "Kami membuat implementasi cicd" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": "Berhasill!!!" }');
+    res.send('{ "response": "Berhasill!!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
